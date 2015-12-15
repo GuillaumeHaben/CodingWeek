7536c91b78b60bc4665c34beda1adf9a00754d30
GuillaumeHaben <pyxouze@gmail.com>
2015-12-15 15:57:15 +0100

    Starting GUI

---
+++ src/controllerFX/MainApp.java
@@ -0,0 +1,79 @@
+package controllerFX;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Coding Bang Fraternity");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.setMinHeight(400);
+            primaryStage.setMinWidth(600);
+            primaryStage.setMaxHeight(400);
+            primaryStage.setMaxWidth(600);
+            primaryStage.getIcons().add(new Image("file:logo.png"));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the home overview inside the root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Load home overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/view/HomeOverview.fxml"));
+            AnchorPane homeOverview = (AnchorPane) loader.load();
+            // Set home overview into the center of root layout.
+            rootLayout.setCenter(homeOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
