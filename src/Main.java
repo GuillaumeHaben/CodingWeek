12bb4eb80786013e71aa229a27a9d5c3513377ef
moulin19u <clemence.moulin@telecomnancy.eu>
2015-12-18 12:44:41 +0100

    Test of the internet connection and Java version

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+
 import controllerFX.MainApp;
 
 /**
@@ -10,17 +16,28 @@ import controllerFX.MainApp;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		// Test of Java's version
+
+		/* Test of Java's version */
 		Double version = Double.parseDouble(System.getProperty("java.specification.version"));
 
 		if (version < 1.8) {
-		    System.out.println("Your version of Java isn't compatible. Please upgrade to Java 1.8 or more.");
-		    return;
+			System.out.println("Your version of Java isn't compatible. Please upgrade to Java 1.8 or more.");
+			return;
 		}
-		
+
+		/* Test the Internet connection */
+		try {
+			URL url = new URL("http://www.google.com");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.connect();
+			if (con.getResponseCode() == 200) {
+			}
+		} catch (Exception exception) {
+			System.out.println("You haven't an Internet connection");
+		}
+
 		// Launch the application in Console or Interface
-		if (args[0] != null) {
+		if (args.length > 0) {
 			if (args[0].compareTo("-console") == 0) {
 				Main_app main_app = new Main_app();
 				main_app.launchApp();
